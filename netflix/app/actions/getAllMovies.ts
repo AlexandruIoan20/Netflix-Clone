6c5143e4d277060bba94fca85ce10a8b5e79e9dd
AlexandruIoan20 <117534646+AlexandruIoan20@users.noreply.github.com>
2023-10-31 13:55:57 +0200

    getAllMovies action

---
+++ netflix/app/actions/getAllMovies.ts
@@ -0,0 +1,18 @@
+import getSession from "./getSession";
+import prismadb from '../../libs/prismadb'; 
+
+const getAllMovies = async  () => { 
+    try { 
+        const session = await getSession(); 
+
+        if(!session?.user?.email)  { 
+            return null 
+        }; 
+
+        const movies = await prismadb.movie.findMany(); 
+        return movies; 
+    } catch(err: any) { 
+        console.log(err); 
+        return null; 
+    }
+}
