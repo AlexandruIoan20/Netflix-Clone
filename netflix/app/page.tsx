bbe64f7354b47a570437a2d5923fd3c1111989f2
AlexandruIoan20 <117534646+AlexandruIoan20@users.noreply.github.com>
2023-10-31 13:51:40 +0200

    billboard random movie

--- netflix/app/page.tsx
+++ netflix/app/page.tsx
@@ -1,10 +1,13 @@
 import Navbar from "@/components/navbar/Navbar"
+import getRandomMovie from "./actions/getRandomMovie"
+import Bilboard from "./components/Bilboard"
 
 const HomePage = async () => {
-
+  const randomMovie = await getRandomMovie(); 
   return (
     <>
       <Navbar /> 
+      <Bilboard movie = { randomMovie! } /> 
     </>
   )
 }
