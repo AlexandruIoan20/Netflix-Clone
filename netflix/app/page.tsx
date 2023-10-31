022d00111f098325a35344a297a0ba2fe3745934
AlexandruIoan20 <117534646+AlexandruIoan20@users.noreply.github.com>
2023-10-31 10:21:39 +0200

    Google and Github auth

--- netflix/app/page.tsx
+++ netflix/app/page.tsx
@@ -1,8 +1,12 @@
-import React from 'react'
+'use client'; 
+
+import { signOut } from "next-auth/react";
 
 const page = () => {
   return (
-    <div className = 'text-center font-semibold'>page</div>
+    <div className = 'text-center font-semibold'>
+      <button onClick={ () => { signOut()}} className = 'bg-white'> log out </button>
+    </div>
   )
 }
 
