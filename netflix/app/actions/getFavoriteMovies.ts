9a39062fba263a96c5c00a8b0d91e021e9a308f5
AlexandruIoan20 <117534646+AlexandruIoan20@users.noreply.github.com>
2023-11-01 14:45:14 +0200

    getfavMovies action

---
+++ netflix/app/actions/getFavoriteMovies.ts
@@ -0,0 +1,32 @@
+import getSession from "./getSession";
+import prismadb from '../../libs/prismadb'; 
+import getCurrentUser from "./getCurrentUser";
+
+const getFavouriteMovies = async () => {
+    try { 
+        const session = await getSession(); 
+        if(!session?.user?.email) { 
+            return []; 
+        }; 
+
+        const currentUser = await getCurrentUser(); 
+        if(!currentUser) { 
+            return []; 
+        }
+
+        const favouriteMovies = await prismadb.movie.findMany({ 
+            where: { 
+                id: { 
+                    in: currentUser?.favouriteIds, 
+                }
+            }
+        }); 
+
+        return favouriteMovies; 
+    } catch(err: any) { 
+        console.log(err); 
+        return []; 
+    }
+}; 
+
+export const getFavouriteMovies; 
---
+++ netflix/app/api/favourite/route.ts
@@ -0,0 +1,67 @@
+import { NextResponse } from "next/server";
+import prismadb from '../../../libs/prismadb';
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import { without } from "lodash";
+
+export const POST = async (request: Request) => { 
+    try { 
+        const { movieId } = await request.json(); 
+        const currentUser = await getCurrentUser(); 
+
+        const existingMovie = await prismadb.movie.findUnique({ 
+            where: { 
+                id: movieId
+            }
+        }); 
+
+        if(!existingMovie) { 
+            return new NextResponse('Not found', { status: 404 }); 
+        }
+
+        const updatedUser = await prismadb.user.update({ 
+            where: { 
+                id: currentUser?.id, 
+            }, data: { 
+                favouriteIds: { 
+                    push: movieId, 
+                }
+            }
+        }); 
+
+        return NextResponse.json(updatedUser); 
+    } catch(err: any) { 
+        console.log(err); 
+        return new NextResponse(err, { status: 500 }); 
+    }
+}
+
+export const DELETE = async (request: Request) => { 
+    try { 
+        const { movieId } = await request.json(); 
+        const currentUser = await getCurrentUser(); 
+
+        const existingMovie = await prismadb.movie.findUnique({ 
+            where: { 
+                id: movieId
+            }
+        }); 
+
+        if(!existingMovie) { 
+            return new NextResponse('Not found', { status: 404 }); 
+        }
+
+        const updateFavoriteIds = without(currentUser?.favouriteIds); 
+        const updatedUser = await prismadb.user.update({ 
+            where: { 
+                id: currentUser?.id, 
+            }, data: { 
+                favouriteIds: updateFavoriteIds, 
+            }
+        }); 
+
+        return NextResponse.json(updatedUser); 
+    } catch(err: any) { 
+        console.log(err); 
+        return new NextResponse(err, { status: 500 }); 
+    }
+}
